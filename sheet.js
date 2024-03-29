0dde3a1469842799b3b2eb00d173668798f596c2
jojoyang <jojoyangbuddy@gmail.com>
2024-03-29 22:13:13 +0800

    update index

--- sheet.js
+++ sheet.js
@@ -18,7 +18,7 @@ checkboxes.forEach(function(checkbox) {
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwhjz0OXaTNTEk-G1QiDC7bs-bdgxijmDSn-9PpVL7GlLO8SfeBrv2iqofIbnBfoxkUYw/exec'
 
 const form = document.forms['contact-form'];
-const submitButton = form.querySelector('input[type="submit"]');
+const submitButton = document.querySelector('input[type="submit"]');
 const statusLabel = document.querySelector('.submited-text');
 
 let hasSubmitted = false;
@@ -29,6 +29,15 @@ form.addEventListener('submit', async (e) => {
   if (hasSubmitted) {
     return;
   }
+  const notAttendingCheckbox = document.getElementById('adot-2');
+  const needInvitation = document.getElementById('idot-1');
+  var addressInput = document.querySelector('input[name="address"]');
+// 获取需要根据复选框状态显示/隐藏的元素
+const additionalElements = document.querySelectorAll('.hideable');
+  let attend = true;
+  if(notAttendingCheckbox.checked)
+    attend = false;
+
   statusLabel.style.display = 'none'
   statusLabel.style.color = 'black'
     const guestSideRadios = form.querySelectorAll('input[name="side"]');
@@ -39,7 +48,7 @@ form.addEventListener('submit', async (e) => {
             }
         });
 
-        if (!guestSideChecked) {
+        if (attend && !guestSideChecked) {
             statusLabel.style.display = 'block'
             statusLabel.style.color = 'red'
             statusLabel.textContent = '請填寫關係欄'
@@ -60,6 +69,26 @@ form.addEventListener('submit', async (e) => {
         return;
     } 
 
+    if (attend ) {
+        var vegetarianSelect = document.getElementById('vegetarian');
+        // 獲取葷食人數的select元素
+        var nonVegetarianSelect = document.getElementById('nonVegetarian');
+
+        // 獲取素食人數和葷食人數的值，並轉換為數字
+        var vegetarianCount = parseInt(vegetarianSelect.value, 10);
+        var nonVegetarianCount = parseInt(nonVegetarianSelect.value, 10);
+
+        // 計算總量
+        var total = vegetarianCount + nonVegetarianCount;
+        if(total==0){
+            statusLabel.style.display = 'block'
+            statusLabel.style.color = 'red'
+            statusLabel.textContent = '請填寫人數'
+            return;
+        }
+        
+    }
+
     const invitationRadios = form.querySelectorAll('input[name="invitation"]');
     let invitationChecked = false;
     invitationRadios.forEach(function(radio) {
@@ -68,12 +97,24 @@ form.addEventListener('submit', async (e) => {
         }
     });
 
-    if (!invitationChecked) {
+    if (attend && !invitationChecked) {
         statusLabel.style.display = 'block'
         statusLabel.style.color = 'red'
         statusLabel.textContent = '請填寫是否需要紙本喜帖'
         return;
     } 
+
+    if(attend && needInvitation.checked) {
+        var addressValue = addressInput.value;
+        // 判斷紙本喜帖寄送地址是否為空白
+        if (addressValue.trim() === '') {
+            statusLabel.style.display = 'block'
+            statusLabel.style.color = 'red'
+            statusLabel.textContent = '請填寫紙本喜帖寄送地址'
+            return;
+        }
+    }
+
 
     const stayRadios = form.querySelectorAll('input[name="stay"]');
     let stayChecked = false;
@@ -83,18 +124,20 @@ form.addEventListener('submit', async (e) => {
         }
     });
 
-    if (!stayChecked) {
+    if (attend && !stayChecked) {
         statusLabel.style.display = 'block'
         statusLabel.style.color = 'red'
         statusLabel.textContent = '請填寫是否需要住宿'
         return;
     } 
+
+    
   hasSubmitted = true;
   submitButton.disabled = true;
   submitButton.classList.add('no-hover');
   statusLabel.style.display = 'block'
   submitButton.value = '提交中'
-  statusLabel.textContent = '提交中 請稍等片刻';
+  statusLabel.innerHTML = '提交中 請稍等片刻';
   
   try {
     const response = await fetch(scriptURL, {
@@ -104,7 +147,13 @@ form.addEventListener('submit', async (e) => {
 
     if (response.ok) {
         statusLabel.style.animation = 'none';
-        statusLabel.textContent = '提交成功 9/7號與您相見';
+        if (!attend ) {
+            statusLabel.innerHTML = '提交成功 感謝您的提交 <img src="buddy_icon3.jpg" alt="Icon" style="width: 50px; height: 50px;">';
+            return;
+        } else{
+            statusLabel.innerHTML = '提交成功 9/7號與您相見 <img src="buddy_icon3.jpg" alt="Icon" style="width: 50px; height: 50px;">';
+
+        }
         form.querySelectorAll('input, textarea').forEach(field => {
         field.disabled = true; // 禁用所有字段
         });
@@ -126,5 +175,55 @@ form.querySelectorAll('input, textarea').forEach(field => {
 });
 
 form.appendChild(statusLabel);
+
+
+// 獲取出席選項元素
+var attendanceOption = document.getElementsByName('attendance');
+// 獲取包含素食人數、葷食人數和兒童座椅數問題的容器元素
+var mealDetails = document.getElementById('meal-details');
+
+var invitationDetails = document.getElementById('invitation-details');
+
+
+var stayDetails = document.getElementById('stay-details');
+
+
+
+// 監聽出席選項的變化
+for (var i = 0; i < attendanceOption.length; i++) {
+  attendanceOption[i].addEventListener('change', function() {
+    // 如果選擇"會出席"，則顯示問題容器，否則隱藏問題容器
+    if (this.value === 'yes') {
+      mealDetails.style.display = 'block';
+      invitationDetails.style.display = 'block'
+      stayDetails.style.display = 'block'
+    } else {
+      mealDetails.style.display = 'none';
+      invitationDetails.style.display = 'none'
+      stayDetails.style.display = 'none'
+    }
+  });
+}
+
+
+var invitationOption = document.getElementsByName('invitation');
+var addressField = document.getElementById('addressField');
+
+// 預設情況下隱藏地址欄位
+addressField.style.display = 'none';
+
+// 監聽是否需要紙本喜帖選項的變化
+for (var i = 0; i < invitationOption.length; i++) {
+  invitationOption[i].addEventListener('change', function() {
+    // 如果選擇"是"，則顯示地址欄位
+    if (this.value === 'yes') {
+      addressField.style.display = 'block';
+    } else {
+      addressField.style.display = 'none';
+    }
+  });
+}
+
+
 
 
